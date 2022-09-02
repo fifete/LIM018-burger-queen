9cc567bfca0178c29170c89d61bb1fd0330ba2db
camotito0 <cecilianallerena@gmail.com>
2022-09-02 12:09:59 -0500

    Añadiendo componente navbar

--- burgerqueen/src/app/app-routing.module.ts
+++ burgerqueen/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
-  { path: "login", component: LoginComponent}
+  { path: "login" , component: LoginComponent},
+  { path: "", redirectTo: "/login" , pathMatch: "full"}
 ];
 
 @NgModule({
